70438f2f2a6e25011de5bfc1bb6ef45af26ecec4
Dana Woltz <dw952717@ohio.edu>
2020-09-01 18:17:13 -0400

    added lines

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Dana Woltz>
+ *        Date: <9-1-2020>
+ * Description: Hello git code 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const* argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
+
